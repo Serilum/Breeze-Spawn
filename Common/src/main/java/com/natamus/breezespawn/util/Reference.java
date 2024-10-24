902e037adf5a308093eccd5af78c31c7b29a81a9
Rick South <rick@natamus.com>
2024-10-24 20:10:36 +0200

    Breeze Spawn source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/breezespawn/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.breezespawn.util;
+
+public class Reference {
+	public static final String MOD_ID = "breezespawn";
+	public static final String NAME = "Breeze Spawn";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
